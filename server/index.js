4088360bf9b91645b219f542350935bb3e0925b7
AneeshRijhwani25 <aneeshrijhwani25@gmail.com>
2024-11-10 12:52:25 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -22,6 +22,9 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB", err));
 
+  app.use("/", (req, res) => {
+    return res.send("Welcome to Chat with Files");
+  });
 // Defining Routes
 app.use("/api/pdf", pdfRoutes);
 
